b32c5d58e4a05a0d879f52ebd07b1c02eb5b1355
IanDoran <59146815+IanDoran@users.noreply.github.com>
2020-01-15 13:56:07 -0500

    Update pe4.js

--- solutions/pe4.js
+++ solutions/pe4.js
@@ -13,24 +13,28 @@ function largestPalindromeProduct(n) {
     var number = parseInt(numb, 10); //this is the largest 'n' digit number
     console.log(number);
     var protopal;
-    var palint = 9009;
-    var pal = palint.toString();
+    //var palint = 9009;
+    //var pal = palint.toString();
     var factor = [];
 
-    //for (i = number; i > 0; i--) {
-        //for (j = number; i > 0; j--){
-            //protopal = i* j; // potential palindrome
-            //var pal = protopal.toString(); // convert to string
+    for (i = number; i > 0; i--) {
+        for (j = number; i > 0; j--){
+            protopal = i* j; // potential palindrome
+            var pal = protopal.toString(); // convert to string
 
             //palindrome check
             for (k = 0; k < n; k++) {
                 if (pal[k] == pal[(2*n) - k - 1]) {
                     console.log(pal[k]);
                     console.log(pal[(2*n) - k - 1]);
-                    factor[l] = [i, j];
-                    console.log(factor[l]);
-                    l++;
-
+                    var counter = 0;
+                    counter++
+                    
+                    if ( counter = n) {
+                        factor[l] = [i, j];
+                        console.log(factor[l]);
+                        l++;
+                    }
                     
                 }
 
@@ -39,8 +43,8 @@ function largestPalindromeProduct(n) {
                 }
             }
 
-        //}
-    //}
+        }
+    }
 
 
 
