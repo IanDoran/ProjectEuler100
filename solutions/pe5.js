1bcc0597607b474bbbded538bce5e014028ee447
IanDoran <59146815+IanDoran@users.noreply.github.com>
2020-01-18 07:27:03 -0500

    Update pe5.js
    Initial solution.

--- solutions/pe5.js
+++ solutions/pe5.js
@@ -1,37 +1,26 @@
 //This function will find the smallest number divisible by consecutive whole numbers from 1 to n.
-//Pseudocode: 
-//For a given number from 1 to x divide by nunbers 1 thru n.
-//If the quotient after dividing by all numbers n are whole numbers the number x is the solution.
-
-
 
 function smallestMult(n) {
-  var x = 0,
-  i = 0,
+  var i = 0,
   j = 0,
-  div = 0,
   smallest = 0,
   counter = 0;
+  var start = new Date().getTime();//TIMER
 
-  function is_int(value){
-  if((parseFloat(value) == parseInt(value)) && !isNaN(value)){
-      return true;
-  } else {
-      return false;
-  }
-}
-
-  func:
-  for (i = 1; i < 1000000000; i++) {
+  for (i = 0; i < 1000000000;) {
     counter = 0;
+    i += n;
     dividendloop:
     for (j = 1; j < n + 1; j++) {
-      div = i / j;
-      if (is_int(div)) {
+      if (i % j === 0) {
         counter++;
         if (counter === (n)) {
           smallest = i;
-          break func;
+          console.log(smallest);
+          var end = new Date().getTime();//TIMER
+          var time = end - start;//TIMER
+          console.log('Execution time: ' + time);//TIMER
+          return smallest;
         }
       }
       else {
@@ -39,8 +28,6 @@ function smallestMult(n) {
       }
     }
   }
-  console.log(smallest);
-  return smallest;
 
 }
 
